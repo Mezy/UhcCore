b26a0b9c833d8e040504ecde111d1951a69081e0
Mezy <mezymc@gmail.com>
2017-12-27 16:34:19 +0100

    Fixed bug where people using 1.12 could not enter the end.

--- src/com/gmail/val59000mc/playuhc/listeners/PortalListener.java
+++ src/com/gmail/val59000mc/playuhc/listeners/PortalListener.java
@@ -17,34 +17,37 @@ public class PortalListener implements Listener{
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	 public void onPlayerPortalEvent (PlayerPortalEvent event){
+
 	     if(GameManager.getGameManager().getConfiguration().getBanNether()){
+
 	    	 event.setCancelled(true);
 	    	 event.getPlayer().sendMessage(ChatColor.RED+Lang.PLAYERS_NETHER_OFF);
+
 	     }else{
+
 	    	 if (event.getTo() == null){
 	             Location loc = event.getFrom();
 	          
 	             if (event.getCause().equals(TeleportCause.NETHER_PORTAL)){
-
-	             	 event.getPortalTravelAgent().setCreationRadius(50);
 	             	 
 	                 if (event.getFrom().getWorld().getEnvironment() == Environment.NETHER){
-	                	 event.getPortalTravelAgent().setSearchRadius(10);
-	                	loc.setWorld(Bukkit.getWorld(GameManager.getGameManager().getConfiguration().getOverworldUuid()));
+	                     loc.setWorld(Bukkit.getWorld(GameManager.getGameManager().getConfiguration().getOverworldUuid()));
 	                     loc.setX( loc.getX()*2d);
 	                     loc.setZ( loc.getZ()*2d);
 	                     event.setTo(event.getPortalTravelAgent().findOrCreate(loc));
 	                 }else{
-	                	 event.getPortalTravelAgent().setSearchRadius(0);
-	                 	 event.getPortalTravelAgent().setCreationRadius(50);
+
 	                	 loc.setWorld(Bukkit.getWorld(GameManager.getGameManager().getConfiguration().getNetherUuid()));
+
 	                     loc.setX( loc.getX()/2d);
 	                     loc.setZ( loc.getZ()/2d);
-	                     event.getPortalTravelAgent().createPortal(loc);
-	                     event.setTo(event.getPortalTravelAgent().findPortal(loc));
+
+	                     event.setTo(event.getPortalTravelAgent().findOrCreate(loc));
+
 	                 }
 	             }
 	         }
 	     }
- }
+
+	}
 }
