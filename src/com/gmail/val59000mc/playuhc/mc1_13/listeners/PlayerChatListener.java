249a527f8939f8f6a98ccbd94a14035598d487ae
Mezy <mezymc@gmail.com>
2018-08-24 22:42:46 +0200

    Changed chat system. No-longer overrides other chat systems

--- src/com/gmail/val59000mc/playuhc/mc1_13/listeners/PlayerChatListener.java
+++ src/com/gmail/val59000mc/playuhc/mc1_13/listeners/PlayerChatListener.java
@@ -2,6 +2,7 @@ package com.gmail.val59000mc.playuhc.mc1_13.listeners;
 
 import com.gmail.val59000mc.playuhc.mc1_13.exceptions.UhcPlayerDoesntExistException;
 import com.gmail.val59000mc.playuhc.mc1_13.game.GameManager;
+import com.gmail.val59000mc.playuhc.mc1_13.players.PlayerState;
 import com.gmail.val59000mc.playuhc.mc1_13.players.UhcPlayer;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -11,36 +12,38 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PlayerChatListener implements Listener{
-	
-	@EventHandler(priority=EventPriority.HIGHEST)
-	public void onPlayerChat(AsyncPlayerChatEvent event){
-		
-		Player player = event.getPlayer();
-		GameManager gm = GameManager.getGameManager();
-		UhcPlayer uhcPlayer;
-		try {
-			uhcPlayer = gm.getPlayersManager().getUhcPlayer(player);
-			switch(uhcPlayer.getState()){
-			case WAITING:
-				event.setCancelled(true);
-				gm.broadcastMessage(ChatColor.WHITE+"<"+ChatColor.AQUA+player.getName()+ChatColor.WHITE+"> "+event.getMessage());
-				break;
-			case PLAYING:
-				event.setCancelled(true);
-				if(uhcPlayer.isGlobalChat())
-					gm.broadcastMessage(ChatColor.WHITE+"<"+ChatColor.AQUA+uhcPlayer.getName()+ChatColor.WHITE+"> "+event.getMessage());
-				else
-					uhcPlayer.getTeam().sendChatMessageToTeamMembers(ChatColor.WHITE+uhcPlayer.getName()+" : "+event.getMessage());
-				break;
-			case DEAD:
-				event.setCancelled(true);
-				if(gm.getConfiguration().getCanSendMessagesAfterDeath())
-					gm.broadcastMessage(ChatColor.GRAY+"[Spec] "+uhcPlayer.getName()+" : "+event.getMessage());
-				break;
-		}
-		} catch (UhcPlayerDoesntExistException e) {
-		}
-				
-		
-	}
+
+    @EventHandler(priority=EventPriority.HIGHEST)
+    public void onPlayerChat(AsyncPlayerChatEvent e){
+        Player player = e.getPlayer();
+        GameManager gm = GameManager.getGameManager();
+
+        if (e.isCancelled()){
+            return;
+        }
+
+        UhcPlayer uhcPlayer;
+        try {
+            uhcPlayer = gm.getPlayersManager().getUhcPlayer(player);
+        } catch (UhcPlayerDoesntExistException ex) {
+            return;
+        }
+
+        // Stop spec chat
+        if(!gm.getConfiguration().getCanSendMessagesAfterDeath() && uhcPlayer.getState() == PlayerState.DEAD){
+            uhcPlayer.sendMessage(ChatColor.RED + "You are not allowed to send messaged!");
+            e.setCancelled(true);
+            return;
+        }
+
+        // Team chat
+        if (!uhcPlayer.isGlobalChat() && uhcPlayer.getState() == PlayerState.PLAYING){
+            e.setCancelled(true);
+            uhcPlayer.getTeam().sendChatMessageToTeamMembers(
+                    ChatColor.WHITE + uhcPlayer.getName() + " : " + e.getMessage()
+            );
+        }
+
+    }
+
 }
