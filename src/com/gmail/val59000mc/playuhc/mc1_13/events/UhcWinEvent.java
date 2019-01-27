5bba857d373f1704147612d5ee50690b57256396
PdeBot <pieterdebot@gmail.com>
2019-01-27 11:21:36 +0100

    Fixed UhcWinEvent

--- src/com/gmail/val59000mc/playuhc/mc1_13/events/UhcWinEvent.java
+++ src/com/gmail/val59000mc/playuhc/mc1_13/events/UhcWinEvent.java
@@ -8,20 +8,25 @@ import java.util.Set;
 
 public class UhcWinEvent extends Event{
 
-	private static final HandlerList handlers = new HandlerList();
+	private static HandlerList handlers = new HandlerList();
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
 	private Set<UhcPlayer> winners;
-	
+
 	public UhcWinEvent(Set<UhcPlayer> winners){
 		this.winners = winners;
 	}
 
 	public Set<UhcPlayer> getWinners(){
 		return winners;
-	}
-	
-	@Override
-	public HandlerList getHandlers() {
-		return handlers;
 	}
 
 }
