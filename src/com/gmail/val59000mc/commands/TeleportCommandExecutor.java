17fe74258ad2fc5c6e3c23c5e46c208ee4824b6f
PdeBot <pieterdebot@gmail.com>
2019-06-29 13:31:43 +0200

    Added permission to override /tp restrictions 'uhc-core.commands.teleport-admin'

--- src/com/gmail/val59000mc/commands/TeleportCommandExecutor.java
+++ src/com/gmail/val59000mc/commands/TeleportCommandExecutor.java
@@ -17,48 +17,57 @@ import org.bukkit.entity.Player;
 public class TeleportCommandExecutor implements CommandExecutor{
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label,
-			String[] args) {
-
-		if(sender instanceof Player){
-			Player player = (Player) sender;
-			GameManager gm = GameManager.getGameManager();
-			PlayersManager pm = gm.getPlayersManager();
-			UhcPlayer uhcPlayer;
-			try {
-				uhcPlayer = pm.getUhcPlayer(player);
-				if(uhcPlayer != null 
-						&& uhcPlayer.getState().equals(PlayerState.DEAD) 
-						&& gm.getGameState().equals(GameState.PLAYING)
-						&& gm.getConfiguration().getSpectatingTeleport()){
-					if(args.length == 1){
-						Player target = Bukkit.getPlayer(args[0]);
-						if(target == null){
-							uhcPlayer.sendMessage(ChatColor.RED+ Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
-						}else{
-							UhcPlayer uhcTarget = pm.getUhcPlayer(target);
-							if(uhcTarget.getState().equals(PlayerState.PLAYING)){
-								uhcPlayer.sendMessage(ChatColor.GREEN+Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", uhcTarget.getName()));
-								player.teleport(target);
-							}else{
-								uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
-							}
-						}
-							
-					}else{
-						uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
-					}
-				}else{
-					uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
-				}
-			} catch (UhcPlayerDoesntExistException e) {
-			}
-
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if (!(sender instanceof Player)){
+			sender.sendMessage("Only players can teleport!");
 			return true;
 		}
-		
 
-		return false;
+		Player player = (Player) sender;
+		GameManager gm = GameManager.getGameManager();
+		PlayersManager pm = gm.getPlayersManager();
+
+		UhcPlayer uhcPlayer;
+		try {
+			uhcPlayer = pm.getUhcPlayer(player);
+
+			if(
+					player.hasPermission("uhc-core.commands.teleport-admin")
+					|| (uhcPlayer.getState().equals(PlayerState.DEAD)
+					&& gm.getGameState().equals(GameState.PLAYING)
+					&& gm.getConfiguration().getSpectatingTeleport())
+			){
+
+				if (args.length != 1){
+					uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
+					return true;
+				}
+
+				Player target = Bukkit.getPlayer(args[0]);
+				if(target == null){
+					uhcPlayer.sendMessage(ChatColor.RED+ Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
+					return true;
+				}
+
+				UhcPlayer uhcTarget = pm.getUhcPlayer(target);
+
+				if(!uhcTarget.getState().equals(PlayerState.PLAYING) && !player.hasPermission("uhc-core.commands.teleport-admin")){
+					uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
+					return true;
+				}
+
+				uhcPlayer.sendMessage(ChatColor.GREEN+Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", uhcTarget.getName()));
+				player.teleport(target);
+				return true;
+			}
+			else{
+				uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
+			}
+		} catch (UhcPlayerDoesntExistException e){
+			// Nothing, player should always exist!
+		}
+
+		return true;
 	}
 
 }
