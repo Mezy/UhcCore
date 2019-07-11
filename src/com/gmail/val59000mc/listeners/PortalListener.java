d2dc03dbaeb94c39302a04f156a9f920ea981eb5
PdeBot <pieterdebot@gmail.com>
2019-07-11 15:51:52 +0200

    Fixed nether always being enabled bug.

--- src/com/gmail/val59000mc/listeners/PortalListener.java
+++ src/com/gmail/val59000mc/listeners/PortalListener.java
@@ -2,6 +2,7 @@ package com.gmail.val59000mc.listeners;
 
 import com.gmail.val59000mc.UhcCore;
 import com.gmail.val59000mc.game.GameManager;
+import com.gmail.val59000mc.languages.Lang;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World.Environment;
@@ -16,7 +17,13 @@ public class PortalListener implements Listener{
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerPortalEvent (PlayerPortalEvent event){
 
-		if(GameManager.getGameManager().getConfiguration().getBanNether() || UhcCore.getVersion() >= 14){
+		if (GameManager.getGameManager().getConfiguration().getBanNether()){
+			event.getPlayer().sendMessage(Lang.PLAYERS_NETHER_OFF);
+			event.setCancelled(true);
+			return;
+		}
+
+		if(UhcCore.getVersion() >= 14){
 			Location loc = event.getFrom();
 
 			if (event.getCause().equals(TeleportCause.NETHER_PORTAL)){
