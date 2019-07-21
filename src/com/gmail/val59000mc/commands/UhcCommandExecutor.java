86dc3cb3a69dac781d75ac2d86df6b51b764874b
PdeBot <pieterdebot@gmail.com>
2019-07-21 17:48:45 +0200

    Changed /uhccore debug command.

--- src/com/gmail/val59000mc/commands/UhcCommandExecutor.java
+++ src/com/gmail/val59000mc/commands/UhcCommandExecutor.java
@@ -1,83 +1,134 @@
 package com.gmail.val59000mc.commands;
 
+import com.gmail.val59000mc.game.GameManager;
 import com.gmail.val59000mc.game.GameState;
 import com.gmail.val59000mc.players.PlayerState;
+import com.gmail.val59000mc.players.PlayersManager;
+import com.gmail.val59000mc.players.UhcPlayer;
+import com.gmail.val59000mc.players.UhcTeam;
 import com.gmail.val59000mc.threads.PreStartThread;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class UhcCommandExecutor implements CommandExecutor{
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label,
-			String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 
-		// For debug purpose
-		if(sender instanceof ConsoleCommandSender || sender instanceof Player){
-			if(sender.hasPermission("uhc-core.commands")){
-				if(args.length > 0){
-					switch(args[0]){
-						case "gamestate":
-							if(args.length == 2){
-								try{
-									CommandManager.setGameState(GameState.valueOf(args[1].toUpperCase()));
-									return true;
-								}catch(IllegalArgumentException e){
-									sender.sendMessage(args[1]+" is not a valid game state");
-								}
-							}
-						break;
-						
-						case "playerstate":
-							if(args.length == 3){
-								try{
-									Player player = Bukkit.getPlayer(args[1]);
-									if(player == null)
-										throw new Exception("Player "+args[1]+" is not online");
-									CommandManager.setPlayerState(player,PlayerState.valueOf(args[2].toUpperCase()));
-									return true;
-								}catch(IllegalArgumentException e){
-									sender.sendMessage(args[2]+" is not a valid player state");
-								}catch(Exception e){
-									sender.sendMessage(e.getMessage());
-								}
-							}
-							
-						case "pvp":
-							if(args.length == 2){
-								CommandManager.setPvp(Boolean.parseBoolean(args[1]));
-								return true;
-							}
-							break;
-							
-						case "listplayers":
-							CommandManager.listUhcPlayers();
-							return true;
-						
-						case "listteams":
-							CommandManager.listUhcTeams();
-							return true;
-							
-						case "pause":
-							String pauseState = PreStartThread.togglePause();
-							sender.sendMessage("The starting thread state is now : "+pauseState);
-							return true;
-							
-						case "force":
-							String forceState = PreStartThread.toggleForce();
-							sender.sendMessage("The starting thread state is now : "+forceState);
-							return true;
-					
-					}
-				}
-			}
-			
+		// debug commands
+		if (!sender.hasPermission("uhc-core.commands.debug")){
+			sender.sendMessage(ChatColor.RED + "You don't have the permission to use UHC-Core debug commands");
+			return true;
 		}
-		return false;
+
+		if (args.length == 0){
+			sender.sendMessage("Invalid command");
+			return true;
+		}
+
+		GameManager gm = GameManager.getGameManager();
+		PlayersManager pm = gm.getPlayersManager();
+
+		switch(args[0]){
+			case "gamestate":
+				if(args.length == 2){
+					try{
+						GameState gameState = GameState.valueOf(args[1].toUpperCase());
+						gm.setGameState(gameState);
+						sender.sendMessage("Changed gamestate to: " + gameState.toString());
+						return true;
+					}catch(IllegalArgumentException e){
+						sender.sendMessage(args[1]+" is not a valid game state");
+						return true;
+					}
+				}else {
+					sender.sendMessage("Current gamestate: " + gm.getGameState());
+					return true;
+				}
+			case "playerstate":
+				if(args.length == 3){
+					try{
+						Player player = Bukkit.getPlayer(args[1]);
+						if(player == null){
+							sender.sendMessage("Player "+args[1]+" is not online");
+							return true;
+						}
+						PlayerState playerState = PlayerState.valueOf(args[2].toUpperCase());
+						pm.getUhcPlayer(player).setState(playerState);
+						sender.sendMessage("Changed " + player.getName() + "'s playerstate to " + playerState);
+						return true;
+					}catch(IllegalArgumentException e){
+						sender.sendMessage(args[2]+" is not a valid player state");
+						return true;
+					}catch(Exception e){
+						sender.sendMessage(e.getMessage());
+						return true;
+					}
+				}else {
+					sender.sendMessage("Invalid playerstate command");
+					return true;
+				}
+
+			case "pvp":
+				if(args.length == 2){
+					boolean state = Boolean.parseBoolean(args[1]);
+					gm.setPvp(state);
+					sender.sendMessage("Changed PvP to " + state);
+					return true;
+				}else {
+					sender.sendMessage("Invalid pvp command");
+					return true;
+				}
+
+			case "listplayers":
+				listUhcPlayers(sender);
+				return true;
+
+			case "listteams":
+				listUhcTeams(sender);
+				return true;
+
+			case "pause":
+				String pauseState = PreStartThread.togglePause();
+				sender.sendMessage("The starting thread state is now : "+pauseState);
+				return true;
+
+			case "force":
+				String forceState = PreStartThread.toggleForce();
+				sender.sendMessage("The starting thread state is now : "+forceState);
+				return true;
+		}
+
+		sender.sendMessage("Unknown sub command " + args[0]);
+		return true;
+	}
+
+
+	private void listUhcPlayers(CommandSender sender) {
+		StringBuilder str = new StringBuilder();
+		str.append("Current UhcPlayers : ");
+		for(UhcPlayer player : GameManager.getGameManager().getPlayersManager().getPlayersList()){
+			str.append(player.getName()+" ");
+		}
+		sender.sendMessage(str.toString());
+	}
+
+	private void listUhcTeams(CommandSender sender) {
+		StringBuilder str;
+		Bukkit.getLogger().info("Current UhcTeams : ");
+
+		for(UhcTeam team : GameManager.getGameManager().getPlayersManager().listUhcTeams()){
+			str = new StringBuilder();
+			str.append("Team "+team.getLeader().getName()+" : ");
+			for(UhcPlayer player : team.getMembers()){
+				str.append(player.getName()+" ");
+			}
+			sender.sendMessage(str.toString());
+		}
 	}
 
 }
