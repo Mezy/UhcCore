f50dfe0851f5e2cde86574e07b0e5b7f3a681f54
PdeBot <pieterdebot@gmail.com>
2019-11-17 17:02:31 +0100

    Added '/teleport player1 player2' command for players with the permission 'uhc-core.commands.teleport-admin'.

--- src/main/java/com/gmail/val59000mc/commands/TeleportCommandExecutor.java
+++ src/main/java/com/gmail/val59000mc/commands/TeleportCommandExecutor.java
@@ -32,56 +32,72 @@ public class TeleportCommandExecutor implements CommandExecutor{
 			uhcPlayer = pm.getUhcPlayer(player);
 
 			if(
-					player.hasPermission("uhc-core.commands.teleport-admin")
-					|| (uhcPlayer.getState().equals(PlayerState.DEAD)
-					&& gm.getConfiguration().getSpectatingTeleport())
+					!player.hasPermission("uhc-core.commands.teleport-admin") &&
+					!(uhcPlayer.getState().equals(PlayerState.DEAD) && gm.getConfiguration().getSpectatingTeleport())
 			){
-
-				if (args.length == 3 && player.hasPermission("uhc-core.commands.teleport-admin")){
-					// teleport to coordinates
-					double x, y, z;
-
-					try {
-						x = Double.parseDouble(args[0]);
-						y = Double.parseDouble(args[1]);
-						z = Double.parseDouble(args[2]);
-					}catch (NumberFormatException ex){
-						sender.sendMessage(ChatColor.RED + "Invalid coordinates!");
-						return true;
-					}
-
-					Location loc = new Location(player.getWorld(), x, y, z);
-					player.teleport(loc);
-
-					player.sendMessage(ChatColor.GREEN+Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", x + "/" + y + "/" + z));
-					return true;
-				}
-
-				if (args.length != 1){
-					uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
-					return true;
-				}
-
-				Player target = Bukkit.getPlayer(args[0]);
-				if(target == null){
-					uhcPlayer.sendMessage(ChatColor.RED+ Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
-					return true;
-				}
-
-				UhcPlayer uhcTarget = pm.getUhcPlayer(target);
-
-				if(!uhcTarget.getState().equals(PlayerState.PLAYING) && !player.hasPermission("uhc-core.commands.teleport-admin")){
-					uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
-					return true;
-				}
-
-				uhcPlayer.sendMessage(ChatColor.GREEN+Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", uhcTarget.getName()));
-				player.teleport(target);
+				uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
 				return true;
 			}
-			else{
-				uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
+
+			if (args.length == 3 && player.hasPermission("uhc-core.commands.teleport-admin")){
+				// teleport to coordinates
+				double x, y, z;
+
+				try {
+					x = Double.parseDouble(args[0]);
+					y = Double.parseDouble(args[1]);
+					z = Double.parseDouble(args[2]);
+				}catch (NumberFormatException ex){
+					sender.sendMessage(ChatColor.RED + "Invalid coordinates!");
+					return true;
+				}
+
+				Location loc = new Location(player.getWorld(), x, y, z);
+				player.teleport(loc);
+
+				player.sendMessage(ChatColor.GREEN+Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", x + "/" + y + "/" + z));
+				return true;
 			}
+
+			if (args.length == 2 && player.hasPermission("uhc-core.commands.teleport-admin")){
+				// teleport player to player
+				Player player1, player2;
+
+				player1 = Bukkit.getPlayer(args[0]);
+				player2 = Bukkit.getPlayer(args[1]);
+
+				if (player1 == null || player2 == null){
+					sender.sendMessage(ChatColor.RED + "That player can not be found!");
+					return true;
+				}
+
+				player1.teleport(player2.getLocation());
+
+				player.sendMessage(ChatColor.GREEN+Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", player1.getName()));
+				return true;
+			}
+
+			if (args.length != 1){
+				uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
+				return true;
+			}
+
+			Player target = Bukkit.getPlayer(args[0]);
+			if(target == null){
+				uhcPlayer.sendMessage(ChatColor.RED+ Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
+				return true;
+			}
+
+			UhcPlayer uhcTarget = pm.getUhcPlayer(target);
+
+			if(!uhcTarget.getState().equals(PlayerState.PLAYING) && !player.hasPermission("uhc-core.commands.teleport-admin")){
+				uhcPlayer.sendMessage(ChatColor.RED+Lang.COMMAND_SPECTATING_TELEPORT_ERROR);
+				return true;
+			}
+
+			uhcPlayer.sendMessage(ChatColor.GREEN+Lang.COMMAND_SPECTATING_TELEPORT.replace("%player%", uhcTarget.getName()));
+			player.teleport(target);
+			return true;
 		} catch (UhcPlayerDoesntExistException e){
 			// Nothing, player should always exist!
 		}
