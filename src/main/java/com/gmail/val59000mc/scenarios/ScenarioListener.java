b4fbd1f3ecec5440e8bc888a59289e472899c185
pieterdebot <pieter.de.bot@merapar.com>
2019-11-21 11:38:43 +0100

    Added methods to get GameManager, PlayerManager & ScoreboardManager.

--- src/main/java/com/gmail/val59000mc/scenarios/ScenarioListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/ScenarioListener.java
@@ -1,12 +1,26 @@
 package com.gmail.val59000mc.scenarios;
 
 import com.gmail.val59000mc.game.GameManager;
+import com.gmail.val59000mc.players.PlayersManager;
+import com.gmail.val59000mc.scoreboard.ScoreboardManager;
 import org.bukkit.event.Listener;
 
 public abstract class ScenarioListener implements Listener{
 
+    public GameManager getGameManager(){
+        return GameManager.getGameManager();
+    }
+
+    public PlayersManager getPlayersManager(){
+        return getGameManager().getPlayersManager();
+    }
+
+    public ScoreboardManager getScoreboardManager(){
+        return getGameManager().getScoreboardManager();
+    }
+
     public ScenarioManager getScenarioManager(){
-        return GameManager.getGameManager().getScenarioManager();
+        return getGameManager().getScenarioManager();
     }
 
     public boolean isActivated(Scenario scenario){
