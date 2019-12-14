1218e2b56e3ce5221388687039b89ed0d21af2bf
pieterdebot <pieter.de.bot@merapar.com>
2019-12-14 22:18:23 +0100

    Added getPlayerName() method to MojangUtils

--- src/main/java/com/gmail/val59000mc/utils/MojangUtils.java
+++ src/main/java/com/gmail/val59000mc/utils/MojangUtils.java
@@ -1,13 +1,16 @@
 package com.gmail.val59000mc.utils;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.bukkit.Bukkit;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.UUID;
 
 public class MojangUtils{
@@ -62,6 +65,29 @@ public class MojangUtils{
         }catch (IOException ex){
             ex.printStackTrace();
             return name;
+        }
+    }
+
+    public static String getPlayerName(UUID uuid){
+        if (Bukkit.isPrimaryThread()){
+            throw new RuntimeException("Requesting player UUID is not allowed on the primary thread!");
+        }
+
+        try {
+            URL url = new URL("https://api.mojang.com/user/profiles/"+uuid.toString().replace("-", "")+"/names");
+
+            URLConnection request = url.openConnection();
+            request.connect();
+
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+
+            JsonArray names = root.getAsJsonArray();
+
+            return names.get(names.size()-1).getAsJsonObject().get("name").getAsString();
+        }catch (IOException ex){
+            ex.printStackTrace();
+            return null;
         }
     }
 
