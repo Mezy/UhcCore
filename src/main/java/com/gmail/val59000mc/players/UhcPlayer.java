b2721f8fe9f50ae5bfdcb9889d79fca060288d2b
PdeBot <pieterdebot@gmail.com>
2019-12-16 15:49:19 +0100

    Fixed bug where when anonymous is enabled your not able to read team chat.

--- src/main/java/com/gmail/val59000mc/players/UhcPlayer.java
+++ src/main/java/com/gmail/val59000mc/players/UhcPlayer.java
@@ -77,7 +77,7 @@ public class UhcPlayer {
 	}
 
 	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
+		this.displayName = displayName + ChatColor.RESET;
 	}
 
 	public boolean hasDisplayName(){
