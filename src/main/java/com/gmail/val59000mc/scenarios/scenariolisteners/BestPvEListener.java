b62727b57752d77393ce9337669f1da81277a6a1
PdeBot <pieterdebot@gmail.com>
2019-12-31 15:15:58 +0100

    Moved scenario threads into scenario listener classes.

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/BestPvEListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/BestPvEListener.java
@@ -2,12 +2,13 @@ package com.gmail.val59000mc.scenarios.scenariolisteners;
 
 import com.gmail.val59000mc.UhcCore;
 import com.gmail.val59000mc.events.UhcStartedEvent;
+import com.gmail.val59000mc.exceptions.UhcPlayerNotOnlineException;
 import com.gmail.val59000mc.game.GameManager;
 import com.gmail.val59000mc.languages.Lang;
 import com.gmail.val59000mc.players.UhcPlayer;
 import com.gmail.val59000mc.scenarios.ScenarioListener;
-import com.gmail.val59000mc.scenarios.threads.BestPvETread;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -17,27 +18,26 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BestPvEListener extends ScenarioListener{
+public class BestPvEListener extends ScenarioListener implements Runnable{
 
-    private BestPvETread bestPvETread;
-    public Map<UhcPlayer,Boolean> pveList;
+    private int taskId;
+    private Map<UhcPlayer,Boolean> pveList;
 
     public BestPvEListener(){
+        taskId = -1;
         pveList = new HashMap<>();
-        bestPvETread = new BestPvETread(this);
     }
 
     @Override
     public void onDisable() {
-        if (bestPvETread.getTaskId() != -1) {
-            Bukkit.getScheduler().cancelTask(bestPvETread.getTaskId());
+        if (taskId != -1) {
+            Bukkit.getScheduler().cancelTask(taskId);
         }
     }
 
     @EventHandler
     public void onGameStart(UhcStartedEvent e) {
-        int taskId = Bukkit.getScheduler().scheduleSyncDelayedTask(UhcCore.getPlugin(), bestPvETread, 12000L);
-        bestPvETread.setTaskId(taskId);
+        taskId = Bukkit.getScheduler().scheduleSyncDelayedTask(UhcCore.getPlugin(), this, 12000L);
 
         for (UhcPlayer uhcPlayer : e.getPlayersManager().getPlayersList()){
             pveList.put(uhcPlayer, true);
@@ -47,7 +47,6 @@ public class BestPvEListener extends ScenarioListener{
 
     @EventHandler (priority = EventPriority.HIGHEST)
     public void onPlayerDamage(EntityDamageEvent e){
-
         if (e.isCancelled()){
             return;
         }
@@ -97,6 +96,35 @@ public class BestPvEListener extends ScenarioListener{
                 uhcPlayer.sendMessage(Lang.SCENARIO_BESTPVE_BACK);
             }
         }
+    }
+
+    @Override
+    public void run() {
+        for (UhcPlayer uhcPlayer : GameManager.getGameManager().getPlayersManager().getOnlinePlayingPlayers()){
+
+            try{
+                Player p = uhcPlayer.getPlayer();
+
+                if (!pveList.containsKey(uhcPlayer)) {
+                    pveList.put(uhcPlayer,true); // Should never occur, playing players are always on list.
+                    Bukkit.getLogger().warning("[UhcCore] " + p.getName() + " was not on best PvE list yet! Please contact a server administrator.");
+                }
+
+                if (p.getGameMode().equals(GameMode.SURVIVAL) && pveList.get(uhcPlayer)){
+                    // heal player
+                    if (p.getHealth() + 2 > p.getMaxHealth()){
+                        p.setMaxHealth(p.getMaxHealth() + 2);
+                    }
+
+                    p.setHealth(p.getHealth() + 2);
+                }
+
+            }catch (UhcPlayerNotOnlineException ex){
+                // No hp for offline players
+            }
+        }
+
+        taskId = Bukkit.getScheduler().scheduleSyncDelayedTask(UhcCore.getPlugin(),this,12000L);
     }
 
 }
--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/TimebombListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/TimebombListener.java
@@ -2,12 +2,20 @@ package com.gmail.val59000mc.scenarios.scenariolisteners;
 
 import com.gmail.val59000mc.UhcCore;
 import com.gmail.val59000mc.scenarios.ScenarioListener;
-import com.gmail.val59000mc.scenarios.threads.TimebombThread;
+import com.gmail.val59000mc.utils.VersionUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
@@ -21,7 +29,81 @@ public class TimebombListener extends ScenarioListener{
         List<ItemStack> drops = new ArrayList<>(e.getDrops());
         e.getDrops().removeAll(e.getDrops());
 
-        Bukkit.getScheduler().scheduleSyncDelayedTask(UhcCore.getPlugin(),new TimebombThread(drops,p.getLocation().getBlock().getLocation(), p.getName()),1L);
+        TimebombThread timebombThread = new TimebombThread(drops, p.getLocation().getBlock().getLocation(), p.getName());
+        Bukkit.getScheduler().scheduleSyncDelayedTask(UhcCore.getPlugin(), timebombThread,1L);
+    }
+
+    public static class TimebombThread implements Runnable{
+
+        private ArmorStand armorStand;
+        private Block block1, block2;
+        private long timeLeft;
+        private Location loc;
+        private List<ItemStack> drops;
+        private String name;
+        private boolean spawned;
+
+        public TimebombThread(List<ItemStack> drops, Location loc, String name){
+            this.drops = drops;
+            this.loc = loc;
+            this.name = name;
+            timeLeft = 30;
+            spawned = false;
+        }
+
+        @Override
+        public void run() {
+            if (!spawned){
+                spawnChest();
+            }
+
+            if (timeLeft > 0){
+                armorStand.setCustomName(ChatColor.GOLD + "" + timeLeft);
+                timeLeft--;
+                Bukkit.getScheduler().scheduleSyncDelayedTask(UhcCore.getPlugin(),this,20L);
+            }else{
+                armorStand.remove();
+                block1.setType(Material.AIR);
+                block2.setType(Material.AIR);
+                block1.getWorld().createExplosion(block1.getLocation(), 10, false);
+            }
+
+        }
+
+        private void spawnChest(){
+            spawned = true;
+
+            block1 = loc.getBlock();
+            loc.add(-1, 0, 0);
+            block2 = loc.getBlock();
+
+            block1.setType(Material.CHEST);
+            block2.setType(Material.CHEST);
+
+            Chest chest1 = (Chest) block1.getState();
+            Chest chest2 = (Chest) block2.getState();
+
+            VersionUtils.getVersionUtils().setChestName(chest1, ChatColor.GOLD + "" + ChatColor.BOLD + name + "'s Timebomb");
+            VersionUtils.getVersionUtils().setChestName(chest2, ChatColor.GOLD + "" + ChatColor.BOLD + name + "'s Timebomb");
+
+            // Make double chest for 1.13 and up
+            VersionUtils.getVersionUtils().setChestSide(chest1, org.bukkit.block.data.type.Chest.Type.RIGHT);
+            VersionUtils.getVersionUtils().setChestSide(chest2, org.bukkit.block.data.type.Chest.Type.LEFT);
+
+            Inventory inv = chest1.getInventory();
+
+            for (ItemStack drop : drops){
+                inv.addItem(drop);
+            }
+
+            loc.add(1,-1,.5);
+
+            armorStand = (ArmorStand) loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
+            armorStand.setCustomNameVisible(true);
+            armorStand.setGravity(false);
+            armorStand.setVisible(false);
+            armorStand.setCustomName("");
+        }
     }
 
 }
