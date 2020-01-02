218a8dfb3b1f8578b11a27b6593847e005ed2243
PdeBot <pieterdebot@gmail.com>
2020-01-02 17:43:22 +0100

    Removed maximum schematic size for 1.8-1.12, though it's still recommended that you don't make them too big as that might crash your server.

--- Support-WorldEdit-6/src/main/java/com/gmail/val59000mc/schematics/SchematicHandler8.java
+++ Support-WorldEdit-6/src/main/java/com/gmail/val59000mc/schematics/SchematicHandler8.java
@@ -13,24 +13,24 @@ import java.io.File;
 import java.util.ArrayList;
 
 @SuppressWarnings("deprecation")
-public class SchematicHandler8 {
+public class SchematicHandler8{
 	
 	public static ArrayList<Integer> pasteSchematic(Location loc, String path) throws Exception{
 		WorldEditPlugin we = (WorldEditPlugin) Bukkit.getPluginManager().getPlugin("WorldEdit");
 		Bukkit.getLogger().info("[UhcCore] Pasting "+path);
 		File schematic = new File(path);
-		EditSession session = we.getWorldEdit().getEditSessionFactory().getEditSession(new BukkitWorld(loc.getWorld()), 1000000);
+		EditSession session = we.getWorldEdit().getEditSessionFactory().getEditSession(new BukkitWorld(loc.getWorld()), -1);
 		
 		CuboidClipboard clipboard = MCEditSchematicFormat.getFormat(schematic).load(schematic);
 		clipboard.paste(session, new Vector(loc.getBlockX(),loc.getBlockY(),loc.getBlockZ()), false);
 
-		ArrayList<Integer> dimensions = new ArrayList<Integer>();
+		ArrayList<Integer> dimensions = new ArrayList<>();
 		dimensions.add(clipboard.getHeight());
 		dimensions.add(clipboard.getLength());
 		dimensions.add(clipboard.getWidth());
 		
 		Bukkit.getLogger().info("[UhcCore] Successfully pasted '"+path+"' at "+loc.getBlockX()+" "+loc.getBlockY()+" "+loc.getBlockZ());
 		return dimensions;
-		
 	}
+
 }
