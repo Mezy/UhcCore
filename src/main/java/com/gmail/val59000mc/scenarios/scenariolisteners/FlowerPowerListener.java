432d8246b9befdd3c85be8b3cb26f7ffb55a9701
PdeBot <pieterdebot@gmail.com>
2020-01-08 19:19:22 +0100

    Fixed bug where FlowerPower would override spawn protection plugins. (Flowers could be broken in protected area)

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/FlowerPowerListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/FlowerPowerListener.java
@@ -11,6 +11,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -81,7 +82,7 @@ public class FlowerPowerListener extends ScenarioListener{
         }
     }
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent e){
         Block block = e.getBlock();
 
