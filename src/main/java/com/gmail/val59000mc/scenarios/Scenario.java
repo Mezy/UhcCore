1367dd4f4be117668dc6ab4ef4c6ea08a527bd6c
Mezy <mezymc@gmail.com>
2020-03-28 12:55:32 +0100

    Finetuned code.

--- src/main/java/com/gmail/val59000mc/scenarios/Scenario.java
+++ src/main/java/com/gmail/val59000mc/scenarios/Scenario.java
@@ -54,7 +54,7 @@ public enum Scenario{
     NOGOINGBACK("No Going Back", UniversalMaterial.NETHER_BRICK, null, "&6No Going Back&7:", "&7- Once you go through a nether portal in the overworld, you can't go back to the overworld."),
     DOUBLEDATES("Double Dates", UniversalMaterial.RED_BANNER, DoubleDatesListener.class, "&6Double Dates&7:", "&7- Players join with a chosen team and are combined randomly with another team."),
     FLYHIGH("Fly High", UniversalMaterial.ELYTRA, FlyHighListener.class, 9, "&6Fly High&7:", "&7- Players spawn with a elytra."),
-    RANDOMIZEDITEMS("Randomized Items", UniversalMaterial.EXPERIENCE_BOTTLE, RandomizedItemsListener.class, "&6Randomized Items&7:", "&7- Every block broken will drop random items." ),
+    RANDOMIZEDDROPS("Randomized Drops", UniversalMaterial.EXPERIENCE_BOTTLE, RandomizedDropsListener.class, "&6Randomized Drops&7:", "&7- Every block broken will drop random items." ),
     UPSIDEDOWNCRAFTING("Upside Down Crafting", UniversalMaterial.CRAFTING_TABLE, UpsideDownCraftsListener.class, 13,"&6Upside Down Crafting&7:", "&7- This scenario flips all crafts upside down.");
 
     private String name;
