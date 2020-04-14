a5c2be5ca8f491322e3c5cc72003547eea8d6bdf
Pierce <33592564+ethankrein@users.noreply.github.com>
2020-04-13 20:58:53 -0700

    Removed unsued imports / cleaned formatting

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/MonstersIncListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/MonstersIncListener.java
@@ -5,10 +5,8 @@ import com.gmail.val59000mc.scenarios.ScenarioListener;
 
 import java.util.List;
 import java.util.HashMap;
-import java.util.Map;
 
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.block.Block;
@@ -51,15 +49,15 @@ public class MonstersIncListener extends ScenarioListener {
 
         if(!block.getType().toString().contains("TRAP") && block.getType().toString().contains("DOOR")) {
             Bisected door = (Bisected) block.getBlockData();
-            if (action == Action.RIGHT_CLICK_BLOCK){
-                if (door.getHalf().toString() == "TOP" || door.getHalf().toString() == "UPPER") {
+            if (action == Action.RIGHT_CLICK_BLOCK) {
+                if (door.getHalf().toString().equals("TOP") || door.getHalf().toString().equals("UPPER")) {
                     block = block.getRelative(BlockFace.DOWN, 1);
                 }
                 if (block.hasMetadata("DoorNum")) {
                     List<MetadataValue> values = block.getMetadata("DoorNum");
                     doorClicked = values.get(0).asInt();
                 }
-                if (DoorsPlaced > 1){
+                if (DoorsPlaced > 1) {
                     do {
                         randomDoor = (int) (Math.random() * DoorLocs.size());
                     } while (randomDoor == doorClicked);
