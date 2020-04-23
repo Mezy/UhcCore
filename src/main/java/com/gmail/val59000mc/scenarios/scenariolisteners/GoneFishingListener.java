37aa984221f2289128fa3a18b31888ce4b941402
Xen0Xys <47594493+Xen0Xys@users.noreply.github.com>
2020-04-23 20:33:30 +0200

    Fix needed change
    Do code optimization

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/GoneFishingListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/GoneFishingListener.java
@@ -1,10 +1,10 @@
 package com.gmail.val59000mc.scenarios.scenariolisteners;
 
+import com.gmail.val59000mc.UhcCore;
 import com.gmail.val59000mc.events.UhcStartedEvent;
 import com.gmail.val59000mc.exceptions.UhcPlayerNotOnlineException;
 import com.gmail.val59000mc.players.UhcPlayer;
 import com.gmail.val59000mc.scenarios.ScenarioListener;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -12,39 +12,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.awt.*;
-
 public class GoneFishingListener extends ScenarioListener{
-
-    public static boolean isMC114(){
-        return Bukkit.getBukkitVersion().contains("1.14");
-    }
-    public static boolean isMC113(){
-        return Bukkit.getBukkitVersion().contains("1.13");
-    }
-    public static boolean isMC112(){
-        return Bukkit.getBukkitVersion().contains("1.12");
-    }
-    public static boolean isMC111(){
-        return Bukkit.getBukkitVersion().contains("1.11");
-    }
-    public static boolean isMC110(){
-        return Bukkit.getBukkitVersion().contains("1.10");
-    }
-
-    public static boolean isMC19(){
-        return Bukkit.getBukkitVersion().contains("1.9");
-    }
-
-    public static boolean isMC18(){
-        return Bukkit.getBukkitVersion().contains("1.8");
-    }
-
-    public static boolean isMC114OrOlder(){
-        if (isMC18() ||isMC19() || isMC110() || isMC111() || isMC112() || isMC113() || isMC114())
-            return true;
-        return false;
-    }
 
     @EventHandler
     public void onGameStarted(UhcStartedEvent e){
@@ -54,14 +22,17 @@ public class GoneFishingListener extends ScenarioListener{
 
         ItemMeta meta = rod.getItemMeta();
         // Set item unbreakable code change in 1.15 and older versions
-        if(isMC114OrOlder()){
+
+        if(UhcCore.getVersion() < 15){
             meta.spigot().setUnbreakable(true);
         }else{
             meta.setUnbreakable(true);
         }
 
-        meta.setDisplayName("" + ChatColor.RED + "The Road");
+        meta.setDisplayName("" + ChatColor.RED + "The Road"); // No need translation, it's a word play with an item name (rod / road)
         rod.setItemMeta(meta);
+
+        ItemStack anvil = new ItemStack(Material.ANVIL, 64);
 
         for (UhcPlayer uhcPlayer : e.getPlayersManager().getOnlinePlayingPlayers()){
             try {
@@ -69,8 +40,7 @@ public class GoneFishingListener extends ScenarioListener{
                 // Give player 10000 xl levels
                 uhcPlayer.getPlayer().setLevel(10000);
                 // Give player 64 anvils
-                ItemStack anvil = new ItemStack(Material.ANVIL);
-                anvil.setAmount(64);
+
                 uhcPlayer.getPlayer().getInventory().addItem(anvil);
             }catch (UhcPlayerNotOnlineException ex){
                 // No rod for offline players
