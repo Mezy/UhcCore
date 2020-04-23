21f240e727bcab12ed761c8a39b407f69d38d109
Xen0Xys <47594493+Xen0Xys@users.noreply.github.com>
2020-04-23 22:04:14 +0200

    Fix gonefishing scenario (#28)
    * Fix gonefishing scenario
    
    Add 64 anvils to player
    Add 100000 xp levels to player
    Add max rod's enchants
    Add version check for maximum compatibility
    Fix work in 1.8.9, no tests on others versions
    
    * Fix needed change
    
    Do code optimization
    
    * Fix code issues
    
    Fix anvils variable name
    Fix blank spaces
    Remove rod custom name
    
    * Update GoneFishingListener.java

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/GoneFishingListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/GoneFishingListener.java
@@ -1,5 +1,6 @@
 package com.gmail.val59000mc.scenarios.scenariolisteners;
 
+import com.gmail.val59000mc.UhcCore;
 import com.gmail.val59000mc.events.UhcStartedEvent;
 import com.gmail.val59000mc.exceptions.UhcPlayerNotOnlineException;
 import com.gmail.val59000mc.players.UhcPlayer;
@@ -8,19 +9,31 @@ import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class GoneFishingListener extends ScenarioListener{
 
     @EventHandler
     public void onGameStarted(UhcStartedEvent e){
         ItemStack rod = new ItemStack(Material.FISHING_ROD);
-        rod.addEnchantment(Enchantment.LURE, 3);
-        rod.addEnchantment(Enchantment.LUCK, 3);
-        rod.addEnchantment(Enchantment.DURABILITY, 3);
-
+        rod.addUnsafeEnchantment(Enchantment.LURE, 8);
+        rod.addUnsafeEnchantment(Enchantment.LUCK, 255);
+        ItemMeta meta = rod.getItemMeta();
+        // Set item unbreakable code change in 1.15 and older versions
+        if(UhcCore.getVersion() < 15){
+            meta.spigot().setUnbreakable(true);
+        }else{
+            meta.setUnbreakable(true);
+        }
+        rod.setItemMeta(meta);
+        ItemStack anvils = new ItemStack(Material.ANVIL, 64);
         for (UhcPlayer uhcPlayer : e.getPlayersManager().getOnlinePlayingPlayers()){
             try {
                 uhcPlayer.getPlayer().getInventory().addItem(rod);
+                // Give player 10000 xl levels
+                uhcPlayer.getPlayer().setLevel(10000);
+                // Give player 64 anvils
+                uhcPlayer.getPlayer().getInventory().addItem(anvils);
             }catch (UhcPlayerNotOnlineException ex){
                 // No rod for offline players
             }
