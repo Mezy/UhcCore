970b9ee4f10bde7ebed69df0d084fc619ed03da6
PdeBot <pieterdebot@gmail.com>
2020-04-25 17:24:56 +0200

    Fixed bug where MonsterInc scenario didn't work on 1.8.

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/MonstersIncListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/MonstersIncListener.java
@@ -10,7 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.Bisected;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -22,16 +21,11 @@ public class MonstersIncListener extends ScenarioListener {
     private List<Location> doorLocs;
 
     public MonstersIncListener(){
-        doorLocs = new ArrayList<Location>();
-    }
-
-    public static boolean isDoor(Block b) {
-        return !b.getType().toString().contains("TRAP") && b.getType().toString().contains("DOOR");
+        doorLocs = new ArrayList<>();
     }
 
     @EventHandler (ignoreCancelled = true)
     public void onBlockPlace(BlockPlaceEvent e) {
-
         Block block = e.getBlock();
         Location loc = e.getBlock().getLocation();
 
@@ -42,38 +36,45 @@ public class MonstersIncListener extends ScenarioListener {
 
     @EventHandler (ignoreCancelled = true)
     public void onBlockClick(PlayerInteractEvent e) {
-
-        Action action = e.getAction();
         Block block = e.getClickedBlock();
         Player player = e.getPlayer();
         Location goToLoc;
 
-        if(isDoor(block)) {
-            Bisected door = (Bisected) block.getBlockData();
-            if (action == Action.RIGHT_CLICK_BLOCK) {
-                if (door.getHalf().toString().equals("TOP")) {
-                    block = block.getRelative(BlockFace.DOWN, 1);
-                }
-                if (doorLocs.size() > 1) {
-                    do {
-                        goToLoc = doorLocs.get((int) (Math.random() * doorLocs.size()));
-                    } while (goToLoc.equals(block.getLocation()));
-                    player.teleport(goToLoc.clone().add(0.5, 0, 0.5));
-                }
-            }
+        if (e.getAction() != Action.RIGHT_CLICK_BLOCK){
+            return;
         }
 
+        if (block == null){
+            return;
+        }
+
+        if(isDoor(block)) {
+            Block below = block.getRelative(BlockFace.DOWN, 1);
+            if (isDoor(below)) {
+                block = below;
+            }
+
+            if (doorLocs.size() > 1) {
+                do {
+                    goToLoc = doorLocs.get((int) (Math.random() * doorLocs.size()));
+                } while (goToLoc.equals(block.getLocation()));
+                player.teleport(goToLoc.clone().add(0.5, 0, 0.5));
+            }
+        }
     }
 
     @EventHandler (ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent e) {
-
         Block block = e.getBlock();
 
         if(isDoor(block)) {
             e.getPlayer().sendMessage(Lang.SCENARIO_MONSTERSINC_ERROR);
             e.setCancelled(true);
         }
-
     }
+
+    private boolean isDoor(Block b) {
+        return !b.getType().toString().contains("TRAP") && b.getType().toString().contains("DOOR");
+    }
+
 }
