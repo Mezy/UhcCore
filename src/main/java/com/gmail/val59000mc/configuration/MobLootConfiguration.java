81306b6aea100c9c38e466907097fec12477595a
PdeBot <pieterdebot@gmail.com>
2020-05-03 12:02:33 +0200

    Mob and Block loot has finally moved to the new JSON item format. This also means you'll now be able to randomize the amount of items that drop using '/iteminfo [min] [max]'.

--- src/main/java/com/gmail/val59000mc/configuration/MobLootConfiguration.java
+++ src/main/java/com/gmail/val59000mc/configuration/MobLootConfiguration.java
@@ -1,5 +1,7 @@
 package com.gmail.val59000mc.configuration;
 
+import com.gmail.val59000mc.exceptions.ParseException;
+import com.gmail.val59000mc.utils.JsonItemUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
@@ -31,12 +33,30 @@ public class MobLootConfiguration {
 		}
 		
 		String itemStr = section.getString("loot");
-		try{
-			String[] itemArr = itemStr.split("/");
-			loot = new ItemStack(Material.valueOf(itemArr[0]), Integer.parseInt(itemArr[1]), Short.parseShort(itemArr[2]));
-		}catch(Exception e){
-			Bukkit.getLogger().warning("[UhcCore] Couldn't parse loot '"+entity.toString()+"' in mob-loot. The syntax must be 'MATERIAL/QUANTITY/DAMAGE'.Ignoring it.");
+
+		if (itemStr == null){
+			Bukkit.getLogger().warning("[UhcCore] Couldn't parse section '"+section.getName()+"' in mob-loot. Missing loot item.");
 			return false;
+		}
+
+		if (itemStr.startsWith("{") && itemStr.endsWith("}")){
+			try {
+				loot = JsonItemUtils.getItemFromJson(itemStr);
+			}catch (ParseException ex){
+				Bukkit.getLogger().warning("[UhcCore] Couldn't parse loot '"+entity.toString()+"' in mob-loot.");
+				ex.printStackTrace();
+				return false;
+			}
+		}else{
+			// TODO: Remove in future update
+			try{
+				String[] itemArr = itemStr.split("/");
+				loot = new ItemStack(Material.valueOf(itemArr[0]), Integer.parseInt(itemArr[1]), Short.parseShort(itemArr[2]));
+				Bukkit.getLogger().warning("[UhcCore] Using old mob-loot syntax, please update loot to json item.");
+			}catch(Exception e){
+				Bukkit.getLogger().warning("[UhcCore] Couldn't parse loot '"+entity.toString()+"' in mob-loot.");
+				return false;
+			}
 		}
 		
 		addXp = section.getInt("add-xp",0);
