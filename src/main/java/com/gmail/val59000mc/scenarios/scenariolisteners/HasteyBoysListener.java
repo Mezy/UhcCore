222dffd57d04a40c9b1d73f032a52856eeabf874
PdeBot <pieterdebot@gmail.com>
2020-12-06 16:05:54 +0100

    HasteyBoys will no longer affect custom crafted tools.

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/HasteyBoysListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/HasteyBoysListener.java
@@ -1,5 +1,6 @@
 package com.gmail.val59000mc.scenarios.scenariolisteners;
 
+import com.gmail.val59000mc.customitems.CraftsManager;
 import com.gmail.val59000mc.scenarios.ScenarioListener;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
@@ -11,6 +12,11 @@ public class HasteyBoysListener extends ScenarioListener{
     @EventHandler
     public void onPlayerCraft(CraftItemEvent e){
         ItemStack item = e.getCurrentItem();
+
+        // Don't apply hastey boy effets to custom crafted items.
+        if (CraftsManager.isCraftItem(item)){
+            return;
+        }
 
         try {
             item.addEnchantment(Enchantment.DIG_SPEED,3);
