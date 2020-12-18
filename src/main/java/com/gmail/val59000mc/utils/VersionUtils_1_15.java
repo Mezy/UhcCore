ad9282659d64c013545aae7c0a51b42ee92f4faa
PdeBot <pieterdebot@gmail.com>
2020-12-18 19:35:51 +0100

    Removed exception log when failing to remove craft.

--- src/main/java/com/gmail/val59000mc/utils/VersionUtils_1_15.java
+++ src/main/java/com/gmail/val59000mc/utils/VersionUtils_1_15.java
@@ -4,7 +4,6 @@ import com.gmail.val59000mc.exceptions.ParseException;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
@@ -23,9 +22,12 @@ public class VersionUtils_1_15 extends VersionUtils_1_14{
         try{
             Method removeRecipe = NMSUtils.getMethod(Bukkit.class, "removeRecipe", NamespacedKey.class);
             boolean removed = (boolean) removeRecipe.invoke(null, item.getType().getKey());
-            Validate.isTrue(removed, "Failed to remove recipe.");
 
-            Bukkit.getLogger().info("[UhcCore] Banned item "+JsonItemUtils.getItemJson(item)+" registered");
+            if (removed){
+                Bukkit.getLogger().info("[UhcCore] Banned item "+JsonItemUtils.getItemJson(item)+" registered");
+            }else {
+                Bukkit.getLogger().info("[UhcCore] Failed to remove recipe " + JsonItemUtils.getItemJson(item) + "!");
+            }
         }catch (Exception ex){
             Bukkit.getLogger().warning("[UhcCore] Failed to register "+JsonItemUtils.getItemJson(item)+" banned craft, make sure your on 1.15.2+!");
             ex.printStackTrace();
