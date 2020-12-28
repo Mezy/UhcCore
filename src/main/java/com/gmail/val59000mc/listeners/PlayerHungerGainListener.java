db9f6f48caf161346ab6c092d7e565a901647618
PdeBot <pieterdebot@gmail.com>
2020-12-28 12:38:18 +0100

    Fixed bug where eating did not work in the deathmatch.

--- src/main/java/com/gmail/val59000mc/listeners/PlayerHungerGainListener.java
+++ src/main/java/com/gmail/val59000mc/listeners/PlayerHungerGainListener.java
@@ -1,23 +1,32 @@
 package com.gmail.val59000mc.listeners;
 
-import com.gmail.val59000mc.game.GameManager;
-import com.gmail.val59000mc.game.GameState;
+import com.gmail.val59000mc.players.PlayerState;
+import com.gmail.val59000mc.players.PlayersManager;
+import com.gmail.val59000mc.players.UhcPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 public class PlayerHungerGainListener implements Listener {
 
-    private final GameManager gameManager;
+    private final PlayersManager playersManager;
 
-    public PlayerHungerGainListener(GameManager gameManager){
-        this.gameManager = gameManager;
+    public PlayerHungerGainListener(PlayersManager playersManager){
+        this.playersManager = playersManager;
     }
 
     @EventHandler
     public void onPlayerHunger(FoodLevelChangeEvent e){
+
+        if (!(e.getEntity() instanceof Player)){
+            return;
+        }
+
+        UhcPlayer player = playersManager.getUhcPlayer((Player) e.getEntity());
+
         // Cancel hunger when the game is not in playing state.
-        if (gameManager.getGameState() != GameState.PLAYING){
+        if (player.getState() != PlayerState.PLAYING){
             e.setCancelled(true);
         }
     }
