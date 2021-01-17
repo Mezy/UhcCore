a34de1d29d443afd3138438fe94f5a64a72858d4
pieterdebot <pieter.de.bot@gmail.com>
2021-01-17 14:29:30 +0100

    Cleaned up code in EnablePermanentDayThread.java.

--- src/main/java/com/gmail/val59000mc/threads/EnablePermanentDayThread.java
+++ src/main/java/com/gmail/val59000mc/threads/EnablePermanentDayThread.java
@@ -4,18 +4,18 @@ import com.gmail.val59000mc.maploader.MapLoader;
 import com.gmail.val59000mc.utils.VersionUtils;
 import org.bukkit.World;
 
-public class EnablePermanentDayThread implements Runnable{
+public class EnablePermanentDayThread implements Runnable {
 
     private final MapLoader mapLoader;
 
-    public EnablePermanentDayThread(MapLoader mapLoader){
+    public EnablePermanentDayThread(MapLoader mapLoader) {
         this.mapLoader = mapLoader;
     }
 
     @Override
     public void run() {
         World overWorld = mapLoader.getUhcWorld(World.Environment.NORMAL);
-        VersionUtils.getVersionUtils().setGameRuleValue(overWorld, "doDaylightCycle", "false");
+        VersionUtils.getVersionUtils().setGameRuleValue(overWorld, MapLoader.DO_DAYLIGHT_CYCLE, false);
         overWorld.setTime(6000);
     }
 
