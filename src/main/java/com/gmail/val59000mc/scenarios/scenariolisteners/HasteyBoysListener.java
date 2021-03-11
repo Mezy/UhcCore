bb7b4436f0fb436570a1d3bc89341145a8739c5f
BelKed <66956532+BelKed@users.noreply.github.com>
2021-03-11 13:55:12 +0100

    Add Stone Tools option for Hastey Boys scenario (#82)
    * Add stone tools option for Hastey Boys scenario

--- src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/HasteyBoysListener.java
+++ src/main/java/com/gmail/val59000mc/scenarios/scenariolisteners/HasteyBoysListener.java
@@ -3,6 +3,8 @@ package com.gmail.val59000mc.scenarios.scenariolisteners;
 import com.gmail.val59000mc.scenarios.Option;
 import com.gmail.val59000mc.customitems.CraftsManager;
 import com.gmail.val59000mc.scenarios.ScenarioListener;
+import com.gmail.val59000mc.utils.UniversalMaterial;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.CraftItemEvent;
@@ -16,13 +18,30 @@ public class HasteyBoysListener extends ScenarioListener{
     @Option(key = "durability")
     private int durability = 1;
 
+    @Option(key = "stone-tools")
+    private boolean stone_tools = false;
+
     @EventHandler
     public void onPlayerCraft(CraftItemEvent e){
         ItemStack item = e.getCurrentItem();
 
-        // Don't apply hastey boy effets to custom crafted items.
+        // Don't apply hastey boy effects to custom crafted items.
         if (CraftsManager.isCraftItem(item)){
             return;
+        }
+
+        if (stone_tools){
+            if (item.getType() == UniversalMaterial.WOODEN_SWORD.getType()){
+                item.setType(Material.STONE_SWORD);
+            }else if (item.getType() == UniversalMaterial.WOODEN_PICKAXE.getType()){
+                item.setType(Material.STONE_PICKAXE);
+            }else if (item.getType() == UniversalMaterial.WOODEN_SHOVEL.getType()){
+                item.setType(UniversalMaterial.STONE_SHOVEL.getType());
+            }else if (item.getType() == UniversalMaterial.WOODEN_AXE.getType()){
+                item.setType(Material.STONE_AXE);
+            }else if (item.getType() == UniversalMaterial.WOODEN_HOE.getType()){
+                item.setType(Material.STONE_HOE);
+            }
         }
 
         try {
