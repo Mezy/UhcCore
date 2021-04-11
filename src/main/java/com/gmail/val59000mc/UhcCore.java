16ce97c95222a60b46ad07292a5c6d34fbbdefbc
IxPrumxI <34715358+IxPrumxI@users.noreply.github.com>
2021-04-11 21:29:25 +0300

    initial commit for discord support

--- src/main/java/com/gmail/val59000mc/UhcCore.java
+++ src/main/java/com/gmail/val59000mc/UhcCore.java
@@ -1,42 +1,37 @@
 package com.gmail.val59000mc;
 
 import com.gmail.val59000mc.game.GameManager;
+import com.gmail.val59000mc.jda.DiscordSRVListener;
 import com.gmail.val59000mc.utils.FileUtils;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import github.scarsz.discordsrv.DiscordSRV;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class UhcCore extends JavaPlugin{
+public class UhcCore extends JavaPlugin {
 
 	private static final int MIN_VERSION = 8;
 	private static final int MAX_VERSION = 17;
 
+	private static EventWaiter eventWaiter;
+	private static DiscordSRV DiscordAPI;
 	private static UhcCore pl;
 	private static int version;
 	private GameManager gameManager;
 	private Updater updater;
+	private boolean discordSupported = false;
 
-	@Override
-	public void onEnable(){
-		pl = this;
-
-		loadServerVersion();
-
-		gameManager = new GameManager();
-		Bukkit.getScheduler().runTaskLater(this, () -> gameManager.loadNewGame(), 1);
-
-		updater = new Updater(this);
-
-		// Delete files that are scheduled for deletion
-		FileUtils.removeScheduledDeletionFiles();
+	public static DiscordSRV getDiscordAPI() {
+		return DiscordAPI;
 	}
 
 	// Load the Minecraft version.
-	private void loadServerVersion(){
+	private void loadServerVersion() {
 		String versionString = Bukkit.getBukkitVersion();
 		version = 0;
 
-		for (int i = MIN_VERSION; i <= MAX_VERSION; i ++){
-			if (versionString.contains("1." + i)){
+		for (int i = MIN_VERSION; i <= MAX_VERSION; i++) {
+			if (versionString.contains("1." + i)) {
 				version = i;
 			}
 		}
@@ -52,15 +47,51 @@ public class UhcCore extends JavaPlugin{
 	public static int getVersion() {
 		return version;
 	}
-	
-	public static UhcCore getPlugin(){
+
+	public static UhcCore getPlugin() {
 		return pl;
 	}
 
+	public static EventWaiter getEventWaiter() {
+		return eventWaiter;
+	}
+
+	public static void setEventWaiter(EventWaiter eventWaiter) {
+		UhcCore.eventWaiter = eventWaiter;
+	}
+
 	@Override
-	public void onDisable(){
+	public void onEnable() {
+		pl = this;
+		DiscordAPI = DiscordSRV.getPlugin();
+		loadServerVersion();
+		DiscordSRV.api.subscribe(new DiscordSRVListener(pl));
+
+		gameManager = new GameManager();
+		Bukkit.getScheduler().runTaskLater(this, () -> gameManager.loadNewGame(), 1);
+
+		updater = new Updater(this);
+
+		// Delete files that are scheduled for deletion
+		FileUtils.removeScheduledDeletionFiles();
+	}
+
+	public boolean isDiscordSupported() {
+		return discordSupported;
+	}
+
+	public void setDiscordSupported(boolean discordSupported) {
+		this.discordSupported = discordSupported;
+	}
+
+	public GameManager getGameManager() {
+		return gameManager;
+	}
+
+	@Override
+	public void onDisable() {
 		gameManager.getScenarioManager().disableAllScenarios();
-		
+
 		updater.runAutoUpdate();
 		Bukkit.getLogger().info("[UhcCore] Plugin disabled");
 	}
