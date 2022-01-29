822735fb088ff99377adec98fca7e9019cf2d7f7
macha-dev <chastonmax@gmail.com>
2022-01-29 11:26:54 +0000

    Added single player heal and heal command permissions.

--- src/main/java/com/gmail/val59000mc/commands/HealCommandExecutor.java
+++ src/main/java/com/gmail/val59000mc/commands/HealCommandExecutor.java
@@ -5,6 +5,7 @@ import com.gmail.val59000mc.game.GameManager;
 import com.gmail.val59000mc.languages.Lang;
 import com.gmail.val59000mc.players.PlayerManager;
 import com.gmail.val59000mc.players.UhcPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -21,15 +22,27 @@ public class HealCommandExecutor implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        for (UhcPlayer uhcPlayer : this.playerManager.getOnlinePlayingPlayers()){
-            try {
-                Player bukkitPlayer = uhcPlayer.getPlayer();
-                bukkitPlayer.setHealth(bukkitPlayer.getMaxHealth());
-            }catch (UhcPlayerNotOnlineException ex){
-                // no heal for offline players
+        if (sender.hasPermission("uhc-core.commands.heal")) {
+            if (args.length == 0) { // heal all players
+                for (UhcPlayer uhcPlayer : this.playerManager.getOnlinePlayingPlayers()) {
+                    try {
+                        Player bukkitPlayer = uhcPlayer.getPlayer();
+                        bukkitPlayer.setHealth(bukkitPlayer.getMaxHealth());
+                    } catch (UhcPlayerNotOnlineException ex) {
+                        // no heal for offline players
+                    }
+                }
+                this.gameManager.broadcastInfoMessage(Lang.GAME_FINAL_HEAL);
+            } else if (args.length == 1) { // heal 1 player
+                Player player = Bukkit.getPlayer(args[0]);
+                if (player != null) { // player found
+                    player.setHealth(player.getMaxHealth());
+                    sender.sendMessage(args[0] + " has been healed.");
+                } else { // player not found
+                    sender.sendMessage(args[0] + " not found.");
+                }
             }
         }
-        this.gameManager.broadcastInfoMessage(Lang.GAME_FINAL_HEAL);
         return true;
     }
 }
